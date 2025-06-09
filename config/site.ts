40f1996088d0c9a8c39e2f65f0a82af625d810a3
PedroBonfim21 <phenrique646@gmail.com>
2025-06-09 20:51:36 -0300

    feat: adicionar configuração do site e utilitário de manipulação de classes

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "UnifBingo",
+  description:
+    "UnifBingo is a bingo game for the Unity community, allowing players to create and play bingo cards based on Unity-related content.",
+};
