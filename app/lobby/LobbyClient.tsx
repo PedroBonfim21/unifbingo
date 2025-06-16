fd2af7ec6387ef08f6ce15ff7c1a81e74460c0c9
PedroBonfim21 <phenrique646@gmail.com>
2025-06-16 16:08:19 -0300

    feat: armazenar ID da sessão do jogo em cookies e localStorage ao iniciar o jogo

--- app/lobby/LobbyClient.tsx
+++ app/lobby/LobbyClient.tsx
@@ -51,6 +51,8 @@ export default function LobbyClient() {
         return;
       }
       const data = await response.json();
+      localStorage.setItem("gameSessionID", data.session);
+      document.cookie = `gameSessionID=${data.session}; path=/;`;
       // Redireciona para a página da sala do jogo usando o room uuid retornado
       router.push(`/room?roomCode=${roomCode}&roomId=${data.room}`);
     } catch {
