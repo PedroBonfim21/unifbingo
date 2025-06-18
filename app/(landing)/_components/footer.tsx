1bf4835800a6833e3be38af4146bb2ba623a4730
PedroBonfim21 <phenrique646@gmail.com>
2025-06-17 23:43:53 -0300

    feat: atualizar estilo do rodapé para melhor apresentação e clareza

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full p-2 border-t bg-slate-100">
+    <div className="fixed bottom-0 w-full p-2 border-t text-white border-purple-800 bg-purple-800">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
