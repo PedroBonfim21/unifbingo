e46ae91049ba025b587ba08c74a31efc222a63a8
PedroBonfim21 <phenrique646@gmail.com>
2025-06-18 19:48:31 -0300

    feat: remover importação dinâmica do RoomClient e simplificar a página de sala

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -1,9 +1,6 @@
-import dynamic from "next/dynamic";
 import ProtectedRoute from "@/components/protectedRoute";
 import { Suspense } from "react";
-
-// Importa dinamicamente o componente cliente com SSR desativado
-const RoomClient = dynamic(() => import("./Roomclient"), { ssr: false });
+import RoomClient from "./Roomclient";
 
 export default function RoomPage() {
   return (
